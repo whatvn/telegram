22747716452f5853e58df0935ff18d5bd21adc04
whatvn <hungnv@opensource.com.vn>
2016-03-04 11:32:45 +0700

    Return error when empty data is posted

--- modules/app.py
+++ modules/app.py
@@ -15,6 +15,7 @@ def apiHandler():
   try:
     logger.info('receive request')
     chat_id = None
+    data = {}
     if request.form.has_key('id'):
       chat_id = request.form['id']
     if request.form.has_key('msg'):
@@ -22,9 +23,11 @@ def apiHandler():
     if text:
       queue.put([chat_id, text])
       data = {'id' : chat_id, 'msg' : text}
-      resp = make_response(json.dumps(data))
-      resp.headers.set('Server', httpwrapper.__version__)
-      resp.headers.set('Content-Type', 'application/json')
-      return resp
+    else:
+      data = {'err' : 1, 'msg' : 'please provide message content'}
+    resp = make_response(json.dumps(data))
+    resp.headers.set('Server', httpwrapper.__version__)
+    resp.headers.set('Content-Type', 'application/json')
+    return resp
   except Exception, ex:
     print ex
